6aa5671d43605269e71d9968c3d9bb99d433f6bb
Marshall Gray <marshallgray1992@gmail.com>
2017-06-20 17:54:51 +1000

    navbar, about complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,12 @@
     })
 
 })(jQuery); // End of use strict
+
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
